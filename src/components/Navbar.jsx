f877d82e8819776e4474cea5077b00275dc09469
Minoo Yaghoubi <155952014+Minoo-YH@users.noreply.github.com>
2026-06-04 11:33:42 +0300

    feat: create navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 const Navbar = () => {
   return (
-    <nav>
-      Navbar
+    <nav className="max-w-7xl mx-auto px-6 py-6 flex items-center justify-between">
+      <div>
+        <h1 className="text-2xl font-bold">Minoo Studio</h1>
+      </div>
+
+      <ul className="hidden md:flex items-center gap-8">
+        <li>Work</li>
+        <li>Services</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+
+      <button className="px-5 py-3 rounded-full bg-purple-200">
+        Let's Talk
+      </button>
     </nav>
   );
 };
